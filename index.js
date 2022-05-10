77e39a9890767c234dde51468a17486ecfd89cdf
perkles <maikosilvae@gmail.com>
2022-05-10 19:35:15 -0300

    2

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function soma(a,b) {
-    return a + b;
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i< array.length; i++ ) {
+    if (array [i] % 2 == 0) {
+        evenNums.push(array[i]);
+    
+    } else {
+        console.log(`${array[i]} nao é par`)
+        ;
+    }    
 }
+console.log('os numeros pares sao ',
+evenNums);
+    }
 
-soma(3,5);
+let array = [1, 2, 4, 5, 7, 8 ];
+
+returnEvenValues(array);
